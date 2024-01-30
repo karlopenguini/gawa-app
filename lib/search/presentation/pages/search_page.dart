32b1ba9dd2c02526fdd8ad2cebce4b6bd2656c22
Gene Anthony <88444596+GeneMachiene@users.noreply.github.com>
2024-01-30 13:10:39 +0800

    Added Search Card | Search page refactor

--- lib/search/presentation/pages/search_page.dart
+++ lib/search/presentation/pages/search_page.dart
@@ -1,5 +1,4 @@
-import 'package:app/home/presentation/pages/home_page.dart';
-import 'package:app/profile/presentation/pages/kagawa_profile_page.dart';
+import 'package:app/search/data/search_list.dart';
 import 'package:flutter/material.dart';
 
 class SearchPage extends StatelessWidget {
@@ -7,6 +6,7 @@ class SearchPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
         body: Padding(
       padding: const EdgeInsets.fromLTRB(13, 32, 13, 0),
@@ -15,178 +15,43 @@ class SearchPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             IconButton(
-              icon: const Icon(Icons.chevron_left, color: Colors.blue),
+              icon: Icon(Icons.chevron_left, color: theme.colorScheme.primary),
               onPressed: () {
                 Navigator.pop(context);
               },
             ),
             IconButton(
-              icon: const Icon(
+              icon: Icon(
                 Icons.location_on,
-                color: Colors.blue,
+                color: theme.colorScheme.primary,
               ),
               onPressed: () {},
             ),
-            const Column(
+            Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text("Home",
                     style: TextStyle(
                         fontSize: 12,
-                        color: Colors.blue,
+                        color: theme.colorScheme.primary,
                         fontWeight: FontWeight.bold,
                         letterSpacing: 1.2)),
                 Text("Blk 3, Lot 21, Solen Residences",
                     style: TextStyle(
-                        fontSize: 12, color: Colors.blue, letterSpacing: 0.8))
+                        fontSize: 12, color: theme.colorScheme.primary, letterSpacing: 0.8))
               ],
             )
           ],
         ),
         const DropdownButtonExample(),
-        GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const KagawaProfilePage(id: 0),
-                ),
-              );
-            },
-            child: const Card(
-              elevation: 0,
-              child: Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Row(children: [
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        backgroundImage: NetworkImage(
-                            'https://pbs.twimg.com/media/E6j3U05XEAE_9Mm.jpg'),
-                        radius: 40,
-                      ),
-                      SizedBox(
-                          // height: 4,
-                          ),
-                      Text("Juan D.C.",
-                          style: TextStyle(
-                              letterSpacing: -1,
-                              fontSize: 14,
-                              color: Colors.black54))
-                    ],
-                  ),
-                  SizedBox(
-                    width: 16,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(children: [
-                        Icon(
-                          Icons.star,
-                          size: 18,
-                          color: Color.fromARGB(255, 255, 209, 59),
-                        ),
-                        SizedBox(
-                          width: 4,
-                        ),
-                        Text("4.9",
-                            style: TextStyle(
-                                fontSize: 14,
-                                letterSpacing: -1,
-                                fontWeight: FontWeight.bold)),
-                        SizedBox(
-                          width: 4,
-                        ),
-                        Text(
-                          "(12 reviews)",
-                          style: TextStyle(
-                              letterSpacing: -1,
-                              fontSize: 12,
-                              color: Colors.black54),
-                        )
-                      ]),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Row(children: [
-                        Icon(Icons.check_circle_outline_rounded,
-                            size: 14, color: Colors.black54),
-                        SizedBox(
-                          width: 4,
-                        ),
-                        Text("100 Gawa Transactions",
-                            style: TextStyle(
-                              letterSpacing: -1,
-                              color: Colors.black54,
-                              fontSize: 12,
-                            ))
-                      ]),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Row(children: [
-                        Icon(Icons.assignment_outlined,
-                            size: 14, color: Colors.black54),
-                        SizedBox(
-                          width: 4,
-                        ),
-                        Text("Plumbing NC II Certified",
-                            style: TextStyle(
-                              letterSpacing: -1,
-                              color: Colors.black54,
-                              fontSize: 12,
-                            )),
-                        SizedBox(
-                          width: 4,
-                        ),
-                        Icon(Icons.copy, size: 12, color: Colors.black54)
-                      ]),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Row(children: [
-                        Icon(Icons.assignment_outlined,
-                            size: 14, color: Colors.black54),
-                        SizedBox(
-                          width: 4,
-                        ),
-                        Text("Pipe Fitting NC II Certified",
-                            style: TextStyle(
-                              letterSpacing: -1,
-                              color: Colors.black54,
-                              fontSize: 12,
-                            )),
-                        SizedBox(
-                          width: 4,
-                        ),
-                        Icon(Icons.copy, size: 12, color: Colors.black54)
-                      ]),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Row(children: [
-                        Icon(Icons.assignment_outlined,
-                            size: 14, color: Colors.black54),
-                        SizedBox(
-                          width: 4,
-                        ),
-                        Text("PESO Binan Accredited",
-                            style: TextStyle(
-                              letterSpacing: -1,
-                              color: Colors.black54,
-                              fontSize: 12,
-                            )),
-                        SizedBox(
-                          width: 4,
-                        ),
-                        Icon(Icons.copy, size: 12, color: Colors.black54)
-                      ]),
-                    ],
-                  )
-                ]),
-              ),
-            ))
+
+        Expanded(
+          child: ListView(
+            padding: const EdgeInsets.all(20),
+            scrollDirection: Axis.vertical,
+              children: searchList,
+          ),
+        ),
       ]),
     ));
   }
---
+++ lib/search/presentation/widgets/kagawa_search_card.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:app/profile/presentation/pages/kagawa_profile_page.dart';
+
+
+class SearchCard extends StatelessWidget {
+  final int id;
+  final double rating;
+  final String name;
+  final String image;
+  final int transactions;
+  final List<String> certs;
+
+  const SearchCard({
+    super.key,
+    required this.id,
+    this.rating = 0.0,
+    required this.name,
+    this.transactions = 0,
+    this.certs = const [],
+    this.image = 'https://th.bing.com/th/id/OIP.ENu9hrlfzOB84klpy9Y20QHaHa?w=198&h=197&c=7&r=0&o=5&pid=1.7',
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const KagawaProfilePage(id: 0),
+            ),
+          );
+        },
+        child: Card(
+          elevation: 0,
+          color: theme.colorScheme.background,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(children: [
+              Column(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(image),
+                    radius: 40,
+                    backgroundColor: Colors.black54,
+                  ),
+                  const SizedBox(
+                    // height: 4,
+                  ),
+                  Text(name,
+                      style: const TextStyle(
+                          letterSpacing: -1,
+                          fontSize: 14,
+                          color: Colors.black54))
+                ],
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(children: [
+                    const Icon(
+                      Icons.star,
+                      size: 18,
+                      color: Colors.amberAccent,
+                    ),
+                    const SizedBox(
+                      width: 4,
+                    ),
+                    Text(rating.toString(),
+                        style: const TextStyle(
+                            fontSize: 14,
+                            letterSpacing: -1,
+                            fontWeight: FontWeight.bold)),
+                    const SizedBox(
+                      width: 4,
+                    ),
+                    Text("(12 reviews)",
+                      style: TextStyle(
+                          letterSpacing: -1,
+                          fontSize: 12,
+                          color: Colors.black54),
+                    )
+                  ]),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Row(children: [
+                    const Icon(Icons.check_circle_outline_rounded,
+                        size: 14, color: Colors.black54),
+                    const SizedBox(
+                      width: 4,
+                    ),
+                    Text("$transactions Gawa Transactions",
+                        style: const TextStyle(
+                          letterSpacing: -1,
+                          color: Colors.black54,
+                          fontSize: 12,
+                        ))
+                  ]),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  for (var cert in certs)
+                    Row(children: [
+                      const Icon(Icons.assignment_outlined,
+                          size: 14, color: Colors.black54),
+                      const SizedBox(
+                        width: 4,
+                      ),
+                      Text(cert,
+                          style: const TextStyle(
+                            letterSpacing: -1,
+                            color: Colors.black54,
+                            fontSize: 12,
+                          )),
+                      const SizedBox(
+                        width: 4,
+                      ),
+                      const Icon(Icons.copy, size: 12, color: Colors.black54)
+                    ]),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                ],
+              )
+            ]),
+          ),
+        ));
+  }
+}
